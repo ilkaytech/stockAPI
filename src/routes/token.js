c3b7082f1d251d5f6b5a78a8fad5ce48362a3aa8
İlkay Öztürk <118361845+ilkaytech@users.noreply.github.com>
2024-06-26 13:28:05 +0300

    token model created

--- src/routes/token.js
+++ src/routes/token.js
@@ -4,3 +4,20 @@
 ------------------------------------------------------- */
 const router = require("express").Router();
 /* ------------------------------------------------------- */
+// routes/Token:
+
+const Token = require("../controllers/token");
+
+// URL: /Tokens
+
+router.route("/").get(Token.list).post(Token.create);
+
+router
+  .route("/:id")
+  .get(Token.read)
+  .put(Token.update)
+  .patch(Token.update)
+  .delete(Token.delete);
+
+/* ------------------------------------------------------- */
+module.exports = router;
